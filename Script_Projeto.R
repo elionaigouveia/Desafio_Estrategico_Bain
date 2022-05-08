def75107ca9282f6b1606fb6287fe523e0e2b88e
Elionai Gouveia <elionaigouveia@gmail.com>
2022-05-08 17:30:12 -0300

    Ordenação das datas prontas!

--- Script_Projeto.R
+++ Script_Projeto.R
@@ -11,19 +11,21 @@ library(lubridate)   #converter datas
 library(readxl) # ler arquivo excel
 library(plyr)   # ordenar a coluna do dataframe
 library(xts)
+
 ################################################################################
 ############################ Importando os dados ###############################
 ################################################################################
-#dados <- read.csv2(file = "historical-database.csv", header = TRUE, sep = ";")
 agriculture <- read_excel("agriculture.xlsx") #Ler o arquivo do excel
 head(agriculture) #visualizar alguns dados
 str(agriculture) # vizualizar como os dados estão sendo armazenados
 #aqui os dados NaN foram excluídos
 agriculture <- data.frame(agriculture[complete.cases(agriculture),]) #limpar NA e transformar em data frame
 colnames(agriculture) <-c("Ano", "Cidade", "Tipo_de_Produto", "Produto", "Area") #atribuicao de nomes das colunas
-#agriculture <- order(rank(agriculture$Ano),) 
-#agriculture <- dmy(agriculture$Ano)
-agriculture[order(rank(agriculture$Ano))]  #colocando as datas em ordem crescente
+agriculture <- agriculture[order(agriculture$Ano, agriculture$Cidade, agriculture$Tipo_de_Produto, agriculture$Produto, decreasing=c(FALSE, FALSE, TRUE,TRUE)), ]
+
+
+#pontos a melhorar
+#1- não retirar os dados NaN, mas substituí-los pela média?? 
 
 ################################################################################
 ################### filtro para cidades, produtos e tipos ######################
@@ -34,12 +36,12 @@ table(agriculture$Cidade)
 table(agriculture$Produto)
 (produtos <- unique(agriculture$Produto))
 
-table(agriculture$`Tipo de Produto`)
-(tipo <- unique(agriculture$`Tipo de Produto`))
+table(agriculture$`Tipo_de_Produto`)
+(tipo <- unique(agriculture$`Tipo_de_Produto`))
 
 nrow(subset(agriculture, agriculture$Cidade==cidades[1] & 
               agriculture$Produto==produtos[1] &
-              agriculture$`Tipo de Produto`==tipo[1]))
+              agriculture$`Tipo_de_Produto`==tipo[1]))
 
 ################################################################################
 ############################## Seperação dos dados ############################# 
@@ -50,11 +52,11 @@ for(j in 1:length(cidades)){
     for(p in 1:length(tipo)){
       if(nrow(subset(agriculture, agriculture$Cidade==cidades[j] & 
                      agriculture$Produto==produtos[i] &
-                     agriculture$`Tipo de Produto`==tipo[p])) >= 1){
+                     agriculture$`Tipo_de_Produto`==tipo[p])) >= 1){
         assign(paste0("Cidade", cidades[j],"Tipo",tipo[p], "Produto", produtos[i]),
                subset(agriculture, agriculture$Cidade==cidades[j] & 
                         agriculture$Produto==produtos[i] &
-                        agriculture$`Tipo de Produto`== tipo[p]))
+                        agriculture$`Tipo_de_Produto`== tipo[p]))
       }
     }
   }
@@ -67,8 +69,8 @@ boxplot(Cidade0307883b5d063703TipotemporaryProdutoOthers[,5])
 ################################################################################
 ############################## Modelo Preditivo ################################
 ################################################################################
-#modelo preditivo para uma tabela
-
+#modelo preditivo para o primeiro dataframe
+#modelo preditivo usando regressão linear e arvore de decisão
 c01 <- Cidade0307883b5d063703TipopastureProdutoLivestock
 
 train_data = c01[c01$Ano <= 2015-01-01,];
@@ -78,29 +80,29 @@ test_data = c01[c01$Ano > 2015-01-01,];
 actual_area <- test_data$Ano;
 
 #Model 1: Use lm to create a linear regression model, trained with the training data set
-model_lm <- lm(c01$Area ~ c01$Cidade + c01$Tipo_de_Produto + c01$Produto, data = train_data);
-
+model_lm <- lm(c01$Area ~  c01$Ano , data = train_data);
 
 #Model 2: Use rpart to create a decision tree model, trained with the training data set
 library(rpart);
-model_rpart  <- rpart(c01$Area ~ c01$Cidade + c01$Tipo_de_Produto + c01$Produto, data = train_data);
+model_rpart  <- rpart(c01$Area ~ c01$Ano , data = train_data);
+model_rpart  <- rpart(c01$Area ~ c01$Ano + $Cidade + c01$Tipo_de_Produto + c01$Produto, data = train_data);
 
 #Use both models to make predictions using the test data set.
 predit_lm <- predict(model_lm, test_data)
 predit_lm <- data.frame(Ano = test_data$Ano, 
-                        Cidade = agriculture$Cidade, 
-                        Tipo_de_Produto = agriculture$Tipo_de_Produto,
-                        Produto = agriculture$Produto,
+                        Cidade = test_data$Cidade, 
+                        Tipo_de_Produto = test_data$Tipo_de_Produto,
+                        Produto = test_data$Produto,
                         Area = test_data$Area,
-                        Area_Pred = predict_lm)
+                        Area_Pred = predit_lm)
 
 predit_rpart  <- predict(model_rpart,  test_data)
 predit_rpart <- data.frame(Ano = test_data$Ano, 
-                           Cidade = agriculture$Cidade, 
-                           Tipo_de_Produto = agriculture$Tipo_de_Produto,
-                           Produto = agriculture$Produto,
+                           Cidade = test_data$Cidade, 
+                           Tipo_de_Produto = test_data$Tipo_de_Produto,
+                           Produto = test_data$Produto,
                            Area = test_data$Area,
-                           Area_Pred = predict_lm)
+                           Area_Pred = predit_rpart)
 
 #To verify it worked, look at the top rows of the two prediction data sets.
 head(predit_lm);
@@ -110,6 +112,8 @@ head(predit_rpart);
 par(mfrow = c(1, 1));
 plot_lm <- plot(predit_lm$Area_Pred - predit_lm$Area, main = "Difference between actual and predicted. lm")
 plot_rp <- plot(predit_rpart$Area_Pred  - predit_rpart$Area,  main = "Difference between actual and predicted. rpart")
+
+#modelo usando séries temporais
 ################################################################################
 ############################### Exportando dados ###############################
 ################################################################################
